ea43566ee2448875d1b39ebf7865a6271dea1893
HayaUp <hayaup.git@gmail.com>
2022-03-15 00:08:42 +0900

    Foodクラスを作成

---
+++ food.js
@@ -0,0 +1,21 @@
+"use strict";
+
+class Food {
+    #name           = "";
+    #price          = 0;
+    #price_range    = 0;
+
+    constructor(name, price, price_range) {
+        this.name = name;
+        this.price = price;
+        this.price_range = price_range;
+    }
+
+    get Name() {
+        return this.name;
+    }
+
+    calculatePrice() {
+        return this.price + Math.random() * this.price_range + 1;
+    }
+}
