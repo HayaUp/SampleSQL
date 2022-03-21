17652db273a2356e39bfcac69351ddf85c0e79ce
HayaUp <hayaup.git@gmail.com>
2022-03-21 20:00:05 +0900

    注文開始・終了日の初期化を追加

--- main.js
+++ main.js
@@ -1,5 +1,31 @@
 "use strict";
 
+// 初期化
+document.addEventListener("DOMContentLoaded", () => {
+    InitializeOrderDate();
+});
+
+// 注文開始・終了日の初期化
+function InitializeOrderDate() {
+    const today = new Date();
+    const firstDay = new Date(today.getFullYear(), today.getMonth() + 1, 1);
+    const startDate = document.getElementById("StartDate");
+    startDate.value = [
+        firstDay.getFullYear(),
+        String(firstDay.getMonth()).padStart(2, "0"),
+        "01"
+    ].join("-");
+
+    const lastDay = new Date(today.getFullYear(), today.getMonth() + 1, 0);
+    const endDate = document.getElementById("EndDate");
+    endDate.value = [
+        lastDay.getFullYear(),
+        String(lastDay.getMonth() + 1).padStart(2, "0"),
+        lastDay.getDate()
+    ].join("-");
+}
+
+// 年月日文字列の作成
 function GenerateSQL() {
     const foods = [
         new Food(1, "apple", 100, 10),
