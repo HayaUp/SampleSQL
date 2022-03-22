b6079f2e702ee2ea92d6a1d0152fc6a85f31c33a
HayaUp <hayaup.git@gmail.com>
2022-03-22 23:18:50 +0900

    コメント修正

--- sql/sampleDML.sql
+++ sql/sampleDML.sql
@@ -9,7 +9,7 @@ FROM
 WHERE
     order_date BETWEEN "2022-03-01" AND "2022-03-05";
 
--- 結合して food_id ではなく Food.name でレコードを表示
+-- 結合して food_id ではなく Foods.name でレコードを表示
 SELECT
     FoodOrders.id,
     Foods.name,
